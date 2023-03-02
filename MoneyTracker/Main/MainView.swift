91e03be79c0bd740808f931c24e7f9cab550218b
Kimleng <kimlenghor2006@gmail.com>
2023-03-02 13:54:09 -0600

    add data persistence -- core data

--- MoneyTracker/Main/MainView.swift
+++ MoneyTracker/Main/MainView.swift
@@ -11,40 +11,144 @@ struct MainView: View {
     
     @State private var shouldPresentAddCardForm = false
     
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @FetchRequest(
+        sortDescriptors: [NSSortDescriptor(keyPath: \Card.timestamp, ascending: false)],
+        animation: .default)
+    private var cards: FetchedResults<Card>
+    
     var body: some View {
         NavigationView {
             ScrollView {
-                TabView {
-                    ForEach(0..<5) { num in
-                        CreditCardView()
-                            .padding(.bottom, 50)
+                if cards.isEmpty {
+                    emptyPromptMessage
+                } else {
+                    TabView {
+                        ForEach(cards) { card in
+                            CreditCardView(card: card)
+                                .padding(.bottom, 50)
+                        }
                     }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
+                    .frame(height: 300)
+                    .indexViewStyle(.page(backgroundDisplayMode: .always))
                 }
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .always))
-                .frame(height: 300)
-                .indexViewStyle(.page(backgroundDisplayMode: .always))
-                
-//                .onAppear {
-//                    shouldPresentAddCardForm.toggle()
-//                }
                 
                 Spacer()
                     .fullScreenCover(isPresented: $shouldPresentAddCardForm) {
                         AddCardForm()
                     }
             }
+            
             .navigationTitle("Credit Cards")
-            .navigationBarItems(trailing: addCardButton)
+            .navigationBarItems(leading: HStack {
+                addItemButton
+                deleteAllButton
+            }, trailing: addCardButton)
+        }
+    }
+    
+    private var emptyPromptMessage: some View {
+        VStack {
+            Text("You currently have no cards in the system.")
+                .padding(.horizontal, 48)
+                .padding(.vertical)
+                .multilineTextAlignment(.center)
+            Button {
+                shouldPresentAddCardForm.toggle()
+            } label: {
+                Text("+ Add Your First Card")
+            }
+            .padding(EdgeInsets(top: 10, leading: 14, bottom: 10, trailing: 14))
+            .foregroundColor(Color(.systemBackground))
+            .background(Color(.label))
+            .cornerRadius(5)
+
+        }.font(.system(size: 22, weight: .semibold))
+    }
+    
+    private var addItemButton: some View {
+        Button(action: {
+            withAnimation {
+                let viewContext = PersistenceController.shared.container.viewContext
+                let card = Card(context: viewContext)
+                card.timestamp = Date()
+
+                do {
+                    try viewContext.save()
+                } catch {
+                    let nsError = error as NSError
+                    fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+                }
+            }
+        }, label: {
+            HStack {
+                Text("Add Item")
+            }
+        })
+    }
+    
+    private var deleteAllButton: some View {
+        Button {
+            cards.forEach { card in
+                viewContext.delete(card)
+            }
+            
+            do {
+                try viewContext.save()
+            } catch {
+                let nsError = error as NSError
+                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
+            }
+        } label: {
+            Text("Delete All")
         }
     }
     
     struct CreditCardView: View {
+        
+        @State private var shouldShowActionSheet = false
+        @State private var shouldShowEditForm = false
+        
+        @State var refreshId = UUID()
+        
+        let card: Card
+        
+        private func handleDelete() {
+            let viewContext = PersistenceController.shared.container.viewContext
+            viewContext.delete(card)
+            do {
+                try viewContext.save()
+            } catch {
+                print("Delete is not working")
+            }
+        }
+        
         var body: some View {
             VStack(alignment: .leading, spacing: 16) {
-                Text("Apple Blue Visa Card")
-                    .font(.system(size: 25, weight: .semibold))
                 HStack {
-                    Image("visa")
+                    Text(card.name ?? "")
+                        .font(.system(size: 25, weight: .semibold))
+                    Spacer()
+                    Button {
+                        shouldShowActionSheet.toggle()
+                    } label: {
+                        Image(systemName: "ellipsis")
+                            .font(.system(size: 28, weight: .bold))
+                    }
+                    .actionSheet(isPresented: $shouldShowActionSheet) {
+                        .init(title: Text(card.name ?? ""), message: Text("Options"), buttons: [
+                            .default(Text("Edit"), action: {
+                                shouldShowEditForm.toggle()
+                            }),
+                            .destructive(Text("Delete Card"), action: handleDelete),
+                            .cancel()
+                        ])
+                    }
+                }
+                HStack {
+                    Image(card.type?.lowercased() ?? "visa")
                         .resizable()
                         .scaledToFit()
                         .frame(height: 44)
@@ -54,23 +158,42 @@ struct MainView: View {
                 }
                 .padding(.bottom, 20)
                 
-                Text("1234 1234 1234 1234")
-                Text("Credit Limit: $50,000")
+                Text(card.number ?? "")
+                
+                HStack {
+                    Text("Credit Limit: $\(card.limit)")
+                    Spacer()
+                    Text("Valid Thru \(card.expMonth < 10 ? ("0" + String(card.expMonth)) : String(card.expMonth))/\(card.expYear % 100)")
+                }
                 
                 HStack { Spacer() }
             }
             .padding()
-            .background(LinearGradient(colors: [.blue.opacity(0.6), .blue], startPoint: .top, endPoint: .bottom))
+            .background(
+                VStack{
+                    if let colorData = card.color,
+                       let uiColor = UIColor.color(data: colorData),
+                       let actualColor = Color(uiColor) {
+                        
+                        LinearGradient(colors: [actualColor.opacity(0.6), actualColor], startPoint: .top, endPoint: .bottom)
+                    } else {
+                        Color.purple
+                    }
+                }
+            )
             .overlay(RoundedRectangle(cornerRadius: 8).stroke(.black.opacity(0.5), lineWidth: 1))
             .foregroundColor(.white)
             .cornerRadius(8)
             .shadow(radius: 5)
             .padding(.horizontal)
             .padding(.top, 8)
+            .fullScreenCover(isPresented: $shouldShowEditForm) {
+                AddCardForm(card: card)
+            }
         }
     }
     
-    var addCardButton: some View {
+    private var addCardButton: some View {
         Button(action: {
             //trigger action
             shouldPresentAddCardForm.toggle()
@@ -87,6 +210,8 @@ struct MainView: View {
 
 struct MainView_Previews: PreviewProvider {
     static var previews: some View {
+        let viewContext = PersistenceController.shared.container.viewContext
         MainView()
+            .environment(\.managedObjectContext, viewContext)
     }
 }
